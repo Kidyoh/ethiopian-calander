3129108b8dd05624391f8e36f607ce2c614e0451
Kidyoh <kidyoh789@gmail.com>
2023-11-03 12:37:14 +0300

    hotfix: ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import 'react-datepicker/dist/react-datepicker.css';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDoubleRight, faAngleLeft, faAngleRight, faAngleDoubleLeft } from '@fortawesome/free-solid-svg-icons';
-
+import ReactSwitch from 'react-switch';
 
 export default function App() {
   const now: EtDatetime = new EtDatetime();
@@ -28,6 +28,7 @@ export default function App() {
   const nextmonth = ethiopianCalendar.nextMonth;
   const previousmonth = ethiopianCalendar.prevMonth;
   const monthDays = ethiopianCalendar.monthDays(true, true);
+  const [isGeez, setIsGeez] = useState(false);
 
   console.log(nextmonth.monthDays(true, true));
   console.log(previousmonth.monthDays(true, true));
@@ -110,12 +111,14 @@ export default function App() {
 
 
   return (
-    <div className='m-0 flex place-items-center min-w-fit min-h-[100vh] items-center justify-center '>
+    <div className='m-0 flex flex-wrap place-items-center min-w-fit min-h-[100vh] items-center justify-center '>
       <div >
         <div style={{ fontSize: '1em', margin: '3px', display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <FontAwesomeIcon icon={faAngleDoubleLeft} onClick={handlePrevYear} style={{ fontSize: '1em', cursor: 'pointer', padding: '30px' }} />
           <FontAwesomeIcon icon={faAngleLeft} onClick={handlePrevMonth} style={{ fontSize: '1em', cursor: 'pointer' , padding:"30px"}} />
-          <h1 style={{ color: "white", flex: 1, textAlign: 'center', margin: '0' }}>{ethiopianMonths[ethiopianCalendar.month - 1]}</h1>
+          <h1 className="text-white flex-1 text-center m-0">
+  {ethiopianMonths[ethiopianCalendar.month - 1]} {ethiopianCalendar.year}
+</h1>
           <FontAwesomeIcon icon={faAngleRight}  size="xl"onClick={handleNextMonth} style={{ fontSize: '1em', cursor: 'pointer', padding: '30px'}} />
           <FontAwesomeIcon icon={faAngleDoubleRight} onClick={handleNextYear} style={{ fontSize: '1em', cursor: 'pointer', padding:"30px" }} />
         </div>
@@ -131,29 +134,33 @@ export default function App() {
          {monthDays.map((day, index) => (
   <div 
     key={index} 
-    style={{
-      border: '1px', 
-      padding: '10px', 
-      color: "black", 
-      fontWeight: 'bold', 
-      display: 'flex', 
-      alignItems: 'center', 
-      justifyContent: 'center',
-      backgroundColor: day[2] === currentEthiopianDate ? 'black' : 'transparent'
-    }}
+    className={`p-2 text-black font-bold flex items-center justify-center 
+    ${day[2] === currentEthiopianDate ? 'bg-black text-white' : 'bg-transparent'}`}
   >
-    {day[2]}
+    {isGeez ? day[2] : index + 1}
   </div>
 ))}
         </div>
+        <br />
+      <div className=' flex items-center space-x-4'>
+      <ReactSwitch 
+  onChange={() => setIsGeez(!isGeez)} 
+  checked={isGeez} 
+  handleDiameter={30} 
+  uncheckedIcon={false} 
+  checkedIcon={false} 
+/>
+<span className={`text-black-700 font-medium`}>{isGeez ? 'Other Numbers' : 'Geez Numbers'}</span>
       </div>
-      <div className='card'>
-        <div className='card-container '>
-          <DatePicker selected={selectedDate} onChange={handleDateChange} />
-          <p>Selected Gregorian date: {gregorianDate}</p>
-          <p>Equivalent Ethiopian date: {ethiopianDate}</p>
-        </div>
       </div>
+      <div className='border border-black bg-white p-5 shadow-md rounded-lg m-2'>
+  <div className='max-w-xs mt-5'>
+    <h1 className='text-purple-700 text-2xl pb-2'>Ethiopian Date Convertor</h1>
+    <DatePicker selected={selectedDate} onChange={handleDateChange} className='w-4/5' />
+    <p className='text-purple-700 text-sm my-2'>Selected Gregorian date: {gregorianDate}</p>
+    <p className='text-purple-700 text-sm my-2'>Equivalent Ethiopian date: {ethiopianDate}</p>
+  </div>
+</div>
 
     </div>
   );
